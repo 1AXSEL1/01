9b020dc222cb24d42d72d20e8a636da69d1679b7
1AXSEL1 <vaskevych-i@class.ztk.org.ua>
2026-01-21 09:12:57 +0000

    jh

---
+++ 2.2.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+int main () {
+
+std::cout << "Візитка студента\n";
+std::cout << "-----------------\n";
+std::cout << "\tПрізвище: Васкевич\n";
+std::cout << "\tІм'я: Іван\n";
+std::cout << "tГруппа: IT-21\n";
+std::cout << "Навчальний заклад: Коледж\n";
+return 0;
+
+}
