d2dad4ad1587f51615d238bc3c69beef28900702
1AXSEL1 <vaskevych-i@class.ztk.org.ua>
2026-01-21 15:57:39 +0200

    Update 2.5.cpp

--- 2.5.cpp
+++ 2.5.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    cout << "Shlyakh do failu: \"C:\\Games\\MyGame\"" << endl;
+    cout << "Punkt 1\tPunkt 2\tPunkt 3" << endl;
+
+    return 0;
+}
